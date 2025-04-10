6cd8e08b4c7c58f07be9370039c2d0c6f10f2e06
Ankita Goyal <goyalankita0325@gmail.com>
2025-04-10 14:50:15 +0530

    done on gfg

--- Arrays/Easy/LargestElement.java
+++ Arrays/Easy/LargestElement.java
@@ -1,5 +1,14 @@
 package Arrays.Easy;
 
 public class LargestElement {
-    
+    public static int largest(int[] arr) {
+        // code here
+        int max = -1;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
